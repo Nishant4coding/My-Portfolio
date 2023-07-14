aedde48d8626d17d5841b4d6fb5f82c4823e9f45
Nishant4coding <srivastava4nishant@gmail.com>
2023-07-14 18:31:56 +0530

    experience

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -6,6 +6,7 @@ function Experience() {
     <div className="main" id="contact">
         <div className="Info-2">
                 <h1>Work Experience</h1>
+            <div className='experience'>
             <div className='exp-card'>
             <img
                 src="./Icons/TULIP.jpg"
@@ -20,6 +21,63 @@ function Experience() {
                 </p>
                 </div>
             </div>
+            <div className='exp-card'>
+            <img
+                src="./Icons/GSSOC.png"
+                alt="Image"
+                className="card-img"
+              />
+              <div className="card-content">
+                <h3 className="card-title">Open Source Contributor</h3>
+                <header>Girl Script Summer of Code 2023</header>
+                <p className="card-text">
+                  May 2023-Present
+                </p>
+                </div>
+            </div>
+            <div className='exp-card'>
+            <img
+                src="./Icons/NASA.jpg"
+                alt="Image"
+                className="card-img"
+              />
+              <div className="card-content">
+                <h3 className="card-title">Front-End Developer (Team Abhedya)</h3>
+                <header>NASA Space Apps Challenge</header>
+                <p className="card-text">
+                  Oct 2022
+                </p>
+                </div>
+            </div>
+            <div className='exp-card'>
+            <img
+                src="./Icons/ECELL.png"
+                alt="Image"
+                className="card-img"
+              />
+              <div className="card-content">
+                <h3 className="card-title">Sr. Member (Technical Team & Event Management)</h3>
+                <header>Entrepreneurship Cell <br /> Galgotias University</header>
+                <p className="card-text">
+                  Sept 2022-Present
+                </p>
+                </div>
+                <div className='exp-card'>
+            <img
+                src="./Icons/SSOC.png"
+                alt="Image"
+                className="card-img"
+              />
+              <div className="card-content">
+                <h3 className="card-title">Open Source Contributor</h3>
+                <header>Social Summer of Code 2023</header>
+                <p className="card-text">
+                  May 2023-Present
+                </p>
+                </div>
+            </div>
+            </div>
+        </div>
         </div>
     </div>
   )

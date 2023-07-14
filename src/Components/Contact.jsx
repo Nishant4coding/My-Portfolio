4da66f33518d8250a8564ccec184e6ede3bb7989
Nishant4coding <srivastava4nishant@gmail.com>
2023-07-14 22:15:04 +0530

    update css

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import "./Intro.css"
 import "./Contact.css"
+import { useForm, ValidationError } from '@formspree/react';
+
 
 
 function Contact() {
+
+  const [state, handleSubmit] = useForm("meqbqwdv");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
 
   const email = 'srivastava4nishant@gmail.com';
   const subject = 'Query from Portfolio';
   const body = 'message';
 
-  const handleEmailButtonClick = () => {
-    window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
-  };
+  // const handleEmailButtonClick = () => {
+  //   window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
+  // };
 
 
   return (
@@ -19,15 +26,46 @@ function Contact() {
       <div className="Info-2">
       <div className="contact-form">
   <span className="heading">Contact Us</span>
-  <form >
-    <label for="name">Name:</label>
-    <input type="text" required=""/>
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required=""/>
-    <label for="message">Message:</label>
-    <textarea id="message" name="message" required=""></textarea>
-     <a><button onClick={handleEmailButtonClick} type="submit">Submit</button></a> 
-  </form>
+  <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <label htmlFor="message">Phone Number</label>
+      <input
+        placeholder="Enter your Contact number"
+        id="mobile"
+        name="mob"
+      />
+      <ValidationError 
+        prefix="mobile" 
+        field="mob"
+        errors={state.errors}
+      />
+      <label htmlFor="message">Message</label>
+      <textarea
+        placeholder="Enter your Message here"
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        <header>Submit</header>
+      </button>
+    </form>
 </div>
 
 

97786b6cfd3582fcc9a0925c6700e3295d0a86ae
Nishant4coding <srivastava4nishant@gmail.com>
2023-07-14 17:34:49 +0530

    experience

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import "./Intro.css"
+import './Experience.css'
 function Experience() {
   return (
     <div className="main" id="contact">
         <div className="Info-2">
                 <h1>Work Experience</h1>
-            <div className='exp'>
-                <ul><li>Web Developer</li>
-                DayNulmm</ul>
-                
+            <div className='exp-card'>
+            <img
+                src="./Projects/TULIP.jpeg"
+                alt="Image"
+                className="card-img"
+              />
+              <div className="card-content">
+                <h3 className="card-title">Web Developer</h3>
+                <header>DAY-NULM TULIP</header>
+                <p className="card-text">
+                  July 2023-Present
+                </p>
+                </div>
             </div>
         </div>
     </div>
